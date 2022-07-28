e8e06e50853e430013b28b62152b1be5814fae93
Filip <filipmcsg@gmail.com>
2022-07-28 12:47:54 +0200

    added db context and entities

---
+++ HotelListing/HotelListingAPI/DAL/Context/HotelListingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HotelListingAPI.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListingAPI.DAL.Context
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
---
+++ HotelListing/HotelListingAPI/DAL/Entities/Hotel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListingAPI.DAL.Entities
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public double Rating { get; set; }
+        public int Capacity { get; set; }
+        [ForeignKey(nameof(CountryId))]
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+    }
+}
